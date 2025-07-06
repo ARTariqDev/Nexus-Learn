b3d3476fcc213284adeb91473d8e32963bfb0169
ARTariqDev <tariqmahmood.dmp@gmail.com>
2025-07-06 06:21:12 +0500

    fixed a spacing issue upon user feedback

--- src/app/home/Alevel/page.js
+++ src/app/home/Alevel/page.js
@@ -32,16 +32,16 @@ export default function HomePage() {
 
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 ">
             <div className="fade-in fade-delay-1 mx-auto">
-              <Resources head="Physics 9702" link="Alevel/Physics" size="3"/>
+              <Resources head="Physics  9702" link="Alevel/Physics" size="3"/>
             </div>
             <div className="fade-in fade-delay-2 mx-auto">
-              <Resources head="Maths 9709" link="Alevel/Maths" size="3"/>
+              <Resources head="Maths  9709" link="Alevel/Maths" size="3"/>
             </div>
             <div className="fade-in fade-delay-3 mx-auto">
-              <Resources head="Computer Science 9618" link="Alevel/CS" size="3"/>
+              <Resources head="ComputerScience  9618" link="Alevel/CS" size="2"/>
             </div>
             <div className="fade-in fade-delay-4 mx-auto">
-              <Resources head="Further Maths 9231" link="Alevel/FM" size="3"/>
+              <Resources head="Further Maths  9231" link="Alevel/FM" size="2"/>
             </div>
             <div className="fade-in fade-delay-4 mx-auto">
               <Resources head="Information Technology 9626" link="Alevel/IT" size="2"/>
--- src/components/resources.js
+++ src/components/resources.js
@@ -11,12 +11,20 @@ const Resources = (props) => {
   const link = "/home/" + props.link;
   const size = props.size ? `text-${props.size}xl` : "text-5xl";
 
+  // Split on double spaces and insert <br /> between
+  const formattedHead = props.head.split("  ").map((part, index, array) => (
+    <span key={index}>
+      {part}
+      {index < array.length - 1 && <br />}
+    </span>
+  ));
+
   return (
     <div className="group bg-[#111111] border-2 border-[#6c6c6c] rounded-2xl p-6 shadow-lg hover:shadow-2xl hover:scale-105 transition-all duration-300 flex flex-col justify-between h-[15rem] w-[20rem] transform">
       <h2
         className={`${monoton.className} ${size} text-[#ffaa00] text-center mb-4 break-words leading-snug mt-4 group-hover:glow transition-all duration-300 py-5`}
       >
-        {props.head}
+        {formattedHead}
       </h2>
       <Link href={link}>
         <button className="mt-auto bg-[#ffaa00] text-black font-semibold px-6 py-2 rounded-xl hover:bg-transparent hover:text-[#ffaa00] hover:border-2 hover:border-[#ffaa00] transition-all w-full">
